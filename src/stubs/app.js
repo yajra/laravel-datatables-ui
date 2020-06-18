50041f4ae1eb2c660a5a8ef48266e0c35e7d3465
Arjay Q. Angeles <aqangeles@gmail.com>
2020-06-18 16:44:07 +0800

    Add notes when to use the custom assets.

--- src/stubs/app.js
+++ src/stubs/app.js
@@ -7,6 +7,11 @@
 require('./bootstrap');
 
 require('./vendor/dataTables.default');
+
+/**
+ * When using DataTable Editor, it is advisable to use the assets below than the buttons.server-side.js script.
+ * Note: buttons.server-side.js have a conflict create button for Editor.
+ */
 require('./vendor/buttons.server-side');
 // require('laravel-datatables-assets/js/dataTables.buttons');
 // require('laravel-datatables-assets/js/dataTables.renderers');
