0497a37b58b7ded5385e8b137243cc52104bc7a2
Arjay Q. Angeles <aqangeles@gmail.com>
2020-06-18 15:58:07 +0800

    Add default x-csrf-token setup for ajax.

--- src/stubs/bootstrap.js
+++ src/stubs/bootstrap.js
@@ -37,6 +37,12 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
